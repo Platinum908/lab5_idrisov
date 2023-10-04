d5dfb14806d062c506bc726587af07034b93edbc
Administrator <Administrator@WIN-QH3366O2A51>
2023-10-04 19:35:01 +0200

    Добавьте файлы проекта.

---
+++ lab5_idrisov/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите c:");
+double c = double.Parse(Console.ReadLine());
+if ((a==b) || (a==c) || (b==c)) {
+    Console.WriteLine("Имеется пара равных");
+} else { Console.WriteLine("Не имеется пара равных"); }
